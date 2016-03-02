f3d4b5cb2c6124d5e410d2abe9756869bf49168e
admin <admin@admindemacbook-pro-2.local>
2016-03-02 19:36:13 +0800

    创建项目

---
+++ wenxin/wenxin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wenxin
+//
+//  Created by admin on 16/3/2.
+//  Copyright © 2016年 LC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ wenxin/wenxin/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  wenxin
+//
+//  Created by admin on 16/3/2.
+//  Copyright © 2016年 LC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
